0fae4cc2b64eae4d28e33cc0c5e454e125c16c00
Chaos99 <stefan@binaervarianz.de>
2011-02-02 14:33:22 +0100

    switched to stack based position trace for parser

--- wordcount.py
+++ wordcount.py
@@ -11,13 +11,9 @@ class pers():
    logcount = 0
    ownlogcount = 0
    ownfoundlogcount = 0
-   wordcount = 0
-   inlogs = False
-   inlogtype = False
-   inlogfinder = False
+   wordcount = 0   
    isown = False
-   isfound = True   
-   inownfoundlogtext = False
+   isfound = True
    username = ''
    stack = []
    
@@ -29,48 +25,31 @@ def start_element(name, attrs):
    if name == 'wpt':      
       pers.count  = pers.count + 1
    if name == 'groundspeak:log':
-      #print 'Start element:', name, attrs
-      pers.inlogs = True
       pers.logcount = pers.logcount + 1   
-   if name == 'groundspeak:finder' and pers.inlogs:
-      pers.inlogfinder = True      
-   if name == 'groundspeak:type' and pers.inlogs:
-      pers.inlogtype = True   
-   if name == 'groundspeak:text' and pers.inlogs and pers.isown and pers.isfound:      
-      pers.inownfoundlogtext = True
-      
-   
-      #print count
-      
+
+
 def end_element(name):
    if pers.stack.pop() != name:
       print "badly formated XML"
    if name == 'groundspeak:log':
-      #print 'End element:', name
-      pers.inlogs = False
       pers.isown = False
       pers.isfound = False
-   if name == 'groundspeak:type' and pers.inlogs:
-      pers.inlogtype = False      
-   if name == 'groundspeak:text':
-      pers.inownfoundlogtext = False
-   if name == 'groundspeak:finder':
-      pers.inlogfinder = False
-    
+
+
 def char_data(data):
-    if pers.inlogtype:       
+    if 'groundspeak:type' in pers.stack:       
        if data == 'Found it' or data == 'Attended':
          pers.isfound = True
          pers.ownfoundlogcount = pers.ownfoundlogcount + 1;
-    if pers.inownfoundlogtext:       
-       countWords(data)
-    if pers.inlogfinder:       
+    if 'groundspeak:log' in pers.stack and pers.stack[-1]=='groundspeak:text':
+       if pers.isown and pers.isfound:
+         countWords(data)
+    if 'groundspeak:log' in pers.stack and pers.stack[-1]=='groundspeak:finder':       
        if data == pers.username:
           pers.isown = True
           pers.ownlogcount = pers.ownlogcount + 1
 
-def countWords(_text):
-   print 'Count'
+def countWords(_text):   
    words = _text.split(None)
    wordcount = len(words)
    pers.wordcount = pers.wordcount + wordcount
